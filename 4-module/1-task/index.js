b8bde6a9d2821ce9da316a4b44a15f8ee01b1c47
LateNever <kirsharik@gmail.com>
2022-11-08 02:25:58 +0500

    module 4 completed

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,12 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  
+  ul = document.createElement('ul');
+  
+  for (let i = 0; i < friends.length; i++) {
+    li = document.createElement('li');
+    li.innerHTML = (friends[i].firstName + ' ' + friends[i].lastName);
+    ul.append(li);
+  }
+
+  return ul
 }
