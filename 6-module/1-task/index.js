1a18e60403803a37178ea4802f715f2b77663aa2
LateNever <kirsharik@gmail.com>
2022-11-29 22:40:55 +0500

    push 6-1, 6-2

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -12,7 +12,63 @@
  *      }
  *
  */
+
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.table = this.render();
   }
+
+  render() {
+    let table = document.createElement('table');
+
+    let thead = document.createElement('thead');
+    thead.innerHTML = (`
+    <tr>
+      <th>Имя</th>
+      <th>Возраст</th>
+      <th>Зарплата</th>
+      <th>Город</th>
+      <th></th>
+    </tr>`);
+    table.appendChild(thead);
+
+    let tbody = document.createElement('tbody');
+    
+
+    for (let i = 0; i < this.rows.length; i++) {
+      let tr = document.createElement('tr');
+
+      for (let key in this.rows[i]) {
+        let td = document.createElement('td');
+        td.innerHTML = this.rows[i][key];
+        tr.appendChild(td);
+      }
+
+      let tdBtn = document.createElement('td');
+      tdBtn.innerHTML = '<button>X</button>';
+      tr.appendChild(tdBtn);
+
+      tbody.appendChild(tr);
+    }
+
+    table.appendChild(tbody);
+
+    table.addEventListener('click', this.onClick);
+
+    return table;
+
+  }
+
+  onClick(ev) {
+    let xBtn = ev.target.closest('button');
+    if (!xBtn) return;
+    ev.target.closest('tr').remove();
+  }
+
+
+  get elem() {
+    return this.table;
+  }
+
 }
--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,57 @@
+import createElement from '../../assets/lib/create-element.js';
+
 export default class ProductCard {
   constructor(product) {
+    this.product = product
+    this.elem = this.render();
   }
+
+  // addBtnOnClick(ev) {
+  //   let addBtn = ev.target.closest('.card__button');
+  //   if (!addBtn) return;
+
+  //   const eventevent = new CustomEvent("product-add", {
+  //     detail: this.product.id,
+  //     bubbles: true
+  //   });
+    
+  //   this.dispatchEvent(eventevent); //нерабочий вариант
+
+  render() {
+
+    const productDiv = createElement(`
+    <div class="card">
+      <div class="card__top">
+          <img src="/assets/images/products/${this.product.image}" class="card__image" alt="product">
+          <span class="card__price">€${this.product.price.toFixed(2)}</span>
+      </div>
+      <div class="card__body">
+          <div class="card__title">${this.product.name}</div>
+          <button type="button" class="card__button">
+              <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+          </button>
+      </div>
+    </div>
+    `);
+      
+    const eventevent = new CustomEvent("product-add", {
+      detail: this.product.id,
+      bubbles: true
+    });
+
+    // productDiv.addEventListener('click', this.addBtnOnClick); //нерабочий вариант
+    productDiv.addEventListener('click', function(ev) {
+
+      let addBtn = ev.target.closest('.card__button');
+      if (!addBtn) return;
+
+      this.dispatchEvent(eventevent);
+
+      console.log(eventevent)
+
+    }); //рабочий вариант
+
+    return productDiv;
+  }
+
 }
